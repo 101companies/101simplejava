ecd89357ba17acdf59248253dfc70548534da236
Thomas Bernau <tbernau@uni-koblenz.de>
2013-06-05 22:25:58 +0200

    copy paste fix

--- contributions/javaLexer/src/main/java/org/softlang/company/features/Parsing.java
+++ contributions/javaLexer/src/main/java/org/softlang/company/features/Parsing.java
@@ -4,10 +4,6 @@ import org.softlang.company.features.parsing.Recognizer;
 
 import java.io.IOException;
 
-/**
- * For clarification, this is precise copy and
- * only shows the idea of Unparsing (noop copy).
- */
 public class Parsing {
 
     public static Recognizer recognizeCompany(String in) throws IOException {

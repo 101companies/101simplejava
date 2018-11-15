498a0fb5e235973d6ba416aa42d0d51732396ab9
Anita <anitaschaefer@uni-koblenz.de>
2018-11-15 18:46:10 +0100

    Formattiert

--- contributions/javaJson/src/main/java/org/softlang/company/features/Parsing.java
+++ contributions/javaJson/src/main/java/org/softlang/company/features/Parsing.java
@@ -7,25 +7,22 @@ import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
 
-public class Parsing
-{
-   /**
-    *
-    * Method reads the given file and convert it to a suitable JSON object; then returns it.
-    *
-    * @param file JSON file with object structure
-    * @return JsonObject with read object structure
-    * @throws IOException
-    */
+public class Parsing {
+    /**
+     * Method reads the given file and convert it to a suitable JSON object; then returns it.
+     *
+     * @param file JSON file with object structure
+     * @return JsonObject with read object structure
+     * @throws IOException
+     */
 
-   public static JsonObject parseCompany(String file) throws IOException
-   {
-      InputStream fis = new FileInputStream(file);
-      JsonReader jsonReader = Json.createReader(fis);
-      JsonObject jsonObject = jsonReader.readObject();
-      jsonReader.close();
-      fis.close();
-      return jsonObject;
-   }
+    public static JsonObject parseCompany(String file) throws IOException {
+        InputStream fis = new FileInputStream(file);
+        JsonReader jsonReader = Json.createReader(fis);
+        JsonObject jsonObject = jsonReader.readObject();
+        jsonReader.close();
+        fis.close();
+        return jsonObject;
+    }
 
 }
